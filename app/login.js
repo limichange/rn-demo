ae1b245683b0c0312f24d3f29ae617b3e23f5e29
limichange <limichange@hotmail.com>
2017-08-12 14:10:55 +0800

    Update

--- app/login.js
+++ app/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,6 +10,7 @@ export default class App extends React.Component {
 
     return (
       <View style={styles.container}>
+        <Image source={require('./home.png')} />
         <Text style={styles.title}>登录</Text>
         <Button
           onPress={() => goBack()}
