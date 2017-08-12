1af0b51d0e6070a90dc5d1320077e826e416833f
limichange <limichange@hotmail.com>
2017-08-12 18:28:24 +0800

    Update

--- app/login.js
+++ app/login.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, Keyboard } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { 
+      username: '',
+      password: ''
+    };
   }
   render() {
     const { goBack } = this.props.navigation
-
+    
     return (
       <View style={styles.container}>
-        <Image source={require('./home.png')} />
         <Text style={styles.title}>登录</Text>
+        <TextInput
+          returnKeyType={'next'}
+          style={{ height: 40 }}
+          placeholder="用户名"
+          onChangeText={(username) => this.setState({ username })}
+          onSubmitEditing={(event) => {
+            this.refs.passwordInput.focus();
+          }}
+        />
+        <TextInput
+          returnKeyType={'done'}
+          ref='passwordInput'
+          style={{ height: 40 }}
+          placeholder="密码"
+          onChangeText={(password) => this.setState({ password })}
+          onSubmitEditing={(event) => {
+            Keyboard.dismiss()
+          }}
+        />
         <Button
           onPress={() => goBack()}
-          title="Go to Brent's profile"
+          title="取消"
         />
       </View>
     );
